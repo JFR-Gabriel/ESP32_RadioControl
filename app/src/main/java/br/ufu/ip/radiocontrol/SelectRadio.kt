b52d6f08d912e61e0dc9ab3496727a6313f61f01
José Gabriel <josegabriel.fagundesribeiro@gmail.com>
2023-03-22 21:07:38 -0300

    First commit

---
+++ app/src/main/java/br/ufu/ip/radiocontrol/SelectRadio.kt
@@ -0,0 +1,13 @@
+package br.ufu.ip.radiocontrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectRadio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_radio)
+    }
+
+}
